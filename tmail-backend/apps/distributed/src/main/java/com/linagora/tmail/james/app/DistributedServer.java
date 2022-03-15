c6483e449fd4c63624b44396ce7cf4c00993a14e
Quan Tran <hqtran@linagora.com>
2022-03-15 10:24:11 +0700

    non break commit

--- tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedServer.java
+++ tmail-backend/apps/distributed/src/main/java/com/linagora/tmail/james/app/DistributedServer.java
@@ -247,6 +247,7 @@ public class DistributedServer {
         if (mailboxConfiguration.isEncryptionEnabled()) {
             return ImmutableList.of(new EncryptedMailboxModule());
         }
+        System.out.println("hello");
         return ImmutableList.of();
     }
 }
