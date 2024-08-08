d9bf7ad4bc5a91c210abd9836dea61a1f332e228
Quan Tran <hqtran@linagora.com>
2024-08-08 10:11:25 +0700

    Sample: modify provisioning.sh

--- tmail-backend/apps/memory/src/main/provisioning/provisioning.sh
+++ tmail-backend/apps/memory/src/main/provisioning/provisioning.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-james-cli AddUser alice@localhost aliceSecret
-james-cli AddUser bob@localhost bobSecret
-james-cli AddUser empty@localhost emptrySecret
+james-cli AddDomain tmail.com
+james-cli AddUser alice@tmail.com aliceSecret
+james-cli AddUser bob@tmail.com bobSecret
+james-cli AddUser empty@tmail.com emptrySecret
 
 declare -a arr=("INBOX" "Important" "tmail" "tmail.mobile" "tmail.backend" "tmail.backend.extensions" "tmail.backend.extensions.pgp" "tmail.backend.extensions.filters" "tmail.backend.extensions.ticketAuth" "tmail.backend.memory" "tmail.backend.distributed" "tmail.marketting" "admin" "customers" "james" "james.dev" "james.user" "james.pmc" "james.dev.gsoc" "Outbox" "Sent" "Drafts" "Trash" "Spam" "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong" "a.b.c.d.e.f.g.h.i.j")
 
 for i in "${arr[@]}"
 do
    echo "Creating mailbox $i"
-   james-cli CreateMailbox \#private alice@localhost $i &
-   james-cli CreateMailbox \#private bob@localhost $i &
+   james-cli CreateMailbox \#private alice@tmail.com $i &
+   james-cli CreateMailbox \#private bob@tmail.com $i &
    wait
 done
 
@@ -19,24 +20,24 @@ do
    for j in {1..41}
    do
        echo "Importing $j.eml in $i"
-       james-cli ImportEml \#private alice@localhost $i /root/provisioning/eml/$j.eml &
-       james-cli ImportEml \#private bob@localhost $i /root/provisioning/eml/$j.eml &
+       james-cli ImportEml \#private alice@tmail.com $i /root/provisioning/eml/$j.eml &
+       james-cli ImportEml \#private bob@tmail.com $i /root/provisioning/eml/$j.eml &
        wait
    done
 done
 
-james-cli CreateMailbox \#private alice@localhost empty
-james-cli CreateMailbox \#private bob@localhost empty
+james-cli CreateMailbox \#private alice@tmail.com empty
+james-cli CreateMailbox \#private bob@tmail.com empty
 
-james-cli CreateMailbox \#private alice@localhost five
-james-cli ImportEml \#private alice@localhost five 0.eml
-james-cli ImportEml \#private alice@localhost five 1.eml
-james-cli ImportEml \#private alice@localhost five 2.eml
-james-cli ImportEml \#private alice@localhost five 3.eml
-james-cli ImportEml \#private alice@localhost five 4.eml
-james-cli CreateMailbox \#private bob@localhost five
-james-cli ImportEml \#private bob@localhost five 0.eml
-james-cli ImportEml \#private bob@localhost five 1.eml
-james-cli ImportEml \#private bob@localhost five 2.eml
-james-cli ImportEml \#private bob@localhost five 3.eml
-james-cli ImportEml \#private bob@localhost five 4.eml
+james-cli CreateMailbox \#private alice@tmail.com five
+james-cli ImportEml \#private alice@tmail.com five 0.eml
+james-cli ImportEml \#private alice@tmail.com five 1.eml
+james-cli ImportEml \#private alice@tmail.com five 2.eml
+james-cli ImportEml \#private alice@tmail.com five 3.eml
+james-cli ImportEml \#private alice@tmail.com five 4.eml
+james-cli CreateMailbox \#private bob@tmail.com five
+james-cli ImportEml \#private bob@tmail.com five 0.eml
+james-cli ImportEml \#private bob@tmail.com five 1.eml
+james-cli ImportEml \#private bob@tmail.com five 2.eml
+james-cli ImportEml \#private bob@tmail.com five 3.eml
+james-cli ImportEml \#private bob@tmail.com five 4.eml
